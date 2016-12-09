18fb1b1571485d6692cd42f48d85848ccaec7a74
ricardo <ricardo@hdlinx.com>
2016-12-09 14:01:21 -0500

    Version retrieval and print

---
+++ getver.py
@@ -0,0 +1,13 @@
+"""
+  userapp
+  AirMapSDK - Example
+
+  Created by AirMap Team on 6/28/16.
+  Copyright (c) 2016 AirMap, Inc. All rights reserved.
+"""
+
+from airmap.airdefs import Globals
+
+ver = Globals()
+print  ver.getVersion () 
+
